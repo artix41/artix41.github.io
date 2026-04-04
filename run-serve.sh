42f52a67c8fe5b0e4819790ad07fbb9351a213b0
Arthur Pesah <arthur.pesah@gmail.com>
2026-04-04 17:12:55 +0200

    Change intro and profile pictures, correct typos, change ruby version

--- run-serve.sh
+++ run-serve.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll serve 2>&1 | egrep -v 'deprecated'
+bundle exec jekyll serve 2>&1 | grep -v 'Deprecation\|deprecated\|More info\|sass-lang\|root stylesheet\|repetitive deprecation\|Run in verbose\|@import\|^[[:space:]]*[│╷╵]'
